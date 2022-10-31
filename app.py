dc78885bff942453565f3310d37ac23a2d8cbef9
Daniel <76977155+iDataEngineer@users.noreply.github.com>
2022-10-31 15:55:32 +0000

    Created app.py

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-from src.snwlnk import app
-app()
+# Streamlit SNWLNK app 
+import src.snwlnk as app
+
+if __name__ == "__main__":
+    # Configure
+    app.LoadCSS(path="css/style.css")
+    app.Celebrate(active=True)
+    
+    # Run
+    app.App(
+        profile = app.configureApp(),
+        profile_pic = "config/profile.jpg")
