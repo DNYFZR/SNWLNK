c9901f86e2f2cdc793f9a5a0e20db1e43db36c9f
Daniel <76977155+iDataEngineer@users.noreply.github.com>
2022-10-31 15:02:32 +0000

    Updated folder structure

--- src/snwlnk.py
+++ src/snwlnk.py
@@ -1,15 +1,16 @@
-import re, time, PIL, streamlit as st
+import re, PIL, sys, streamlit as st
+sys.path.append('..')
 from utils.tools import st_button, load_css
 
 # Load CSS style
-load_css(path="css/style.css")
+load_css(path="../css/style.css")
 
 # Celebrate   
 st.snow()
 
 
 # Bring in data from profiles.yml
-with open("input/profile.yml", "rb") as profiles:
+with open("../config/profile.yml", "rb") as profiles:
     # Read in, decode & remove chars 
     profiles = profiles.read().decode()
     profiles = profiles.replace('"', '').replace('\r', '').split('\n')
@@ -22,7 +23,7 @@ st.markdown(f'''**❄ SNWLNK ❄**''')
 
 # App page
 col1, col2, col3 = st.columns(3)
-col2.image(PIL.Image.open("input/profile.jpg"))
+col2.image(PIL.Image.open("../config/profile.jpg"))
 
 # Person Info
 st.header(f'''{profiles['name']}''')
@@ -33,8 +34,8 @@ if "info" in profiles.keys():
         
 # Links
 for key in profiles.keys():
-    if key in ['name', 'info']:
-        pass
-    else:
+    if key not in ['name', 'info']:
         print(key, profiles[key])
         st_button(label=key, url=profiles[key])
+
+st.markdown('---')
